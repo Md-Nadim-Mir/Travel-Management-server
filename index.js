9ee4b6339c5da46744f841cfacbeac27399f22af
Md-Nadim-Mir <mdnadimmir150@gmail.com>
2024-06-26 13:08:41 +0600

    route create

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ app.get('/',(req,res)=>{
      res.send(`server is running port in ${port}`);
 })
 
+// users management routes start 
+
+// ----> user read 
+app.get('/users',(req,res)=>{
+    res.send(users)
+})
+
+// ---> user create
+app.post('/users',(req,res)=>{
+    console.log('data posted');
+    console.log(req.body);
+})
+// users management routes end
+
 
 app.listen(port,()=>{
     console.log(`PORT is running ${port}`)
