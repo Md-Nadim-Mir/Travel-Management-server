694568d66414bed60caad4484ce6fbdb1337e8b5
Md-Nadim-Mir <mdnadimmir150@gmail.com>
2024-06-26 12:18:36 +0600

    index.js file create

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000 ;
+
+app.get('/',(req,res)=>{
+     console.log('server is running');
+})
+
+
+app.listen(port,()=>{
+    console.log(`PORT is running ${port}`)
+})
