27d6840952a7be7df60ad2f35a695568798f1f0c
Md-Nadim-Mir <mdnadimmir150@gmail.com>
2024-06-27 21:02:54 +0600

    deleted api create successfully

--- index.js
+++ index.js
@@ -137,6 +137,16 @@ async function run() {
        
     })
 
+    //  <----------- Places : DElETE method>
+    app.delete('/places/:id',async(req,res)=>{
+         
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result =await placesCollection.deleteOne(query);
+        res.send(result);
+       
+    })
+
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
